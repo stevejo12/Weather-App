74bedb97d44fedb4dc9dc1e498c228d9a93e48ef
stevejo12 <37164804+stevejo12@users.noreply.github.com>
2021-09-06 13:19:02 +0700

    Simple Weather App using openweathermap

---
+++ src/constants.js
@@ -0,0 +1,26 @@
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+export { months, days };
